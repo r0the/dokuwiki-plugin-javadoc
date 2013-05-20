b70f8ff9743f9ad234aa964336c85d332d17fe52
Stefan Rothe <info@stefan-rothe.ch>
2013-05-20 06:42:52 +0200

    Improved comments.

--- syntax.php
+++ syntax.php
@@ -1,18 +1,15 @@
 <?php
 /**
- * Add Javadoc link capability to dokuwiki.
+ * Add Javadoc link capability to dokuwiki
  *
- * by Damien Coraboeuf <dcoraboeuf@yahoo.fr>
- * under the terms of the GNU GPL v2.
- *
- * @license    GNU_GPL_v2
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     Damien Coraboeuf <dcoraboeuf@yahoo.fr>
+ * @author     Stefan Rothe <info@stefan-rothe.ch>
  */
 
 if(!defined('DOKU_INC')) define('DOKU_INC',realpath(dirname(__FILE__).'/../../').'/');
 if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 require_once(DOKU_PLUGIN.'syntax.php');
-
 
 class syntax_plugin_javadoc extends DokuWiki_Syntax_Plugin {
 
@@ -27,10 +24,16 @@ class syntax_plugin_javadoc extends DokuWiki_Syntax_Plugin {
         );
     }
 
+    /**
+     * What kind of syntax are we?
+     */
     function getType() {
         return 'substition';
     }
 
+    /**
+     * What about paragraphs?
+     */
     function getPType() {
         return 'normal';
     }
@@ -39,12 +42,16 @@ class syntax_plugin_javadoc extends DokuWiki_Syntax_Plugin {
         return array('container','substition','protected','disabled','formatting','paragraphs');
     }
 
+    /**
+     * Where to sort in?
+     */
     function getSort() {
         return 194;
     }
 
-    // override default accepts() method to allow nesting
-    // - ie, to get the plugin accepts its own entry syntax
+    /**
+     * Allow nesting, i.e. let the plugin accept its own entry syntax
+     */
     function accepts($mode) {
         if ($mode == substr(get_class($this), 7)) {
             return true;
@@ -54,14 +61,23 @@ class syntax_plugin_javadoc extends DokuWiki_Syntax_Plugin {
         }
     }
 
+    /**
+     * Connect pattern to lexer
+     */
     function connectTo($mode) {
         $this->Lexer->addEntryPattern('<javadoc.*?>(?=.*?</javadoc>)',$mode,'plugin_javadoc');
     }
 
+    /**
+     * Add exit pattern to lexer
+     */
     function postConnect() {
         $this->Lexer->addExitPattern('</javadoc>','plugin_javadoc');
     }
 
+    /**
+     * Handle the match
+     */
     function handle($match, $state, $pos, &$handler){
         switch ($state) {
             case DOKU_LEXER_ENTER :
@@ -79,6 +95,9 @@ class syntax_plugin_javadoc extends DokuWiki_Syntax_Plugin {
         }
     }
 
+    /**
+     * Create output
+     */
     function render($mode, &$renderer, $indata) {
         $sites = array (
             'jdk6' => 'http://java.sun.com/javase/6/docs/api',
